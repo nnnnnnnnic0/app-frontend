73bf936fd6b3c8f901059cb7578418e70ec1f1ee
nnnnnnnnic0 <ser.reyesv@gmail.com>
2025-05-23 22:26:14 -0400

    Footer1.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import Servicios from '../components/Servicios'
 import Sobre from '../components/Sobre'
 import Contacto from '../components/Contacto'
-import Footer from '../components/Footer'
+import Footer from '../components/Footer1'
 
 export default function Home() {
     return (
