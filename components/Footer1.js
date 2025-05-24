4c019a840c6bfc1647104f45c222979c8e4daae8
nnnnnnnnic0 <ser.reyesv@gmail.com>
2025-05-23 22:38:57 -0400

    borrador de header y footer

--- components/Footer1.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-blue-900 text-white py-6 mt-10">
-            <div className="container mx-auto text-center text-sm">
-                © {new Date().getFullYear()} ReyesVallejos · Todos los derechos reservados.
-            </div>
-        </footer>
-    );
-}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import Header from '../components/Header1'
 import Hero from '../components/Hero'
 import Servicios from '../components/Servicios'
 import Sobre from '../components/Sobre'
 import Contacto from '../components/Contacto'
-import Footer from '../components/Footer1'
 
 export default function Home() {
     return (
