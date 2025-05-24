91294359327a344e4fa03f4c546a47d7ef2e39b4
nnnnnnnnic0 <ser.reyesv@gmail.com>
2025-05-23 22:30:58 -0400

    Problema rutas con mayusculas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Header from '../components/Header'
+import Header from '../components/Header1'
 import Hero from '../components/Hero'
 import Servicios from '../components/Servicios'
 import Sobre from '../components/Sobre'
