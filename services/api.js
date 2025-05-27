7a26c7455c741f1e6a2e1c6c27b605902a924e9d
nnnnnnnnic0 <ser.reyesv@gmail.com>
2025-05-27 16:53:24 -0400

    conexion elastic backend

--- services/api.js
+++ services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_AUTH_API_BASE || "http://localhost:8000";
 
 export async function ping() {
     const res = await fetch(`${API_BASE}/ping`);
